52079ace1b43870b28739cfabf4815ba0b5d7777
Muhamad Rizki <rizshuffle@gmail.com>
2026-02-09 16:16:48 +0700

    Django RT RW Net

--- web/urls.py
+++ web/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import index
+from . import views
 
 urlpatterns = [
-    path('', index, name='home'),
+    path('', views.home, name='home'),
+    path('pasang/', views.pricing, name='pricing'),
+    path('pelanggan/', views.pelanggan, name='pelanggan'),
+    path('pelanggan/bayar/', views.bayar, name='bayar'),
+    path('pelanggan/gangguan/', views.gangguan, name='gangguan'),
 ]
--- web/views.py
+++ web/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 
-def index(request):
-    paket = [
-        {"speed": 10, "price": 155},
-        {"speed": 15, "price": 175},
-        {"speed": 20, "price": 205},
-        {"speed": 30, "price": 255},
-    ]
-    return render(request, 'web/index.html', {'paket': paket})
+def home(request):
+    return render(request, 'web/home.html')
+
+def pricing(request):
+    return render(request, 'web/pricing.html')
+
+def pelanggan(request):
+    return render(request, 'web/pelanggan.html')
+
+def bayar(request):
+    return render(request, 'web/bayar.html')
+
+def gangguan(request):
+    return render(request, 'web/gangguan.html')
